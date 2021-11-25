af1d265be5d371502ab1a8e906fab5fa8739061a
shwetamasrani <shwetamasrani19@gmail.com>
2021-11-25 18:52:19 +0530

    Login fields modified

--- src/test/java/com/iiitb/sportitup/ByPassTest.java
+++ src/test/java/com/iiitb/sportitup/ByPassTest.java
@@ -1,32 +1,38 @@
 package com.iiitb.sportitup;
-
+import java.lang.Thread;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ByPassTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/home/ubuntu/Documents/Testing/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:3000/userSignInComponent");
         //Locating the email field element via Name tag and storing it in the webelement
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
         WebElement email_field = driver.findElement(By.name("emailId"));
 
+        String typeValue = email_field.getAttribute("type");
         //Entering text into the email field
         email_field.sendKeys("shwetamasrani19@gmail.com");
-
+        Thread.sleep(3000);
         //Locating the password field element via Name tag and storing it in the webelement
         WebElement password_field=driver.findElement(By.name("password"));
-
+        js.executeScript
+                ("document.getElementsByName('password')[0].setAttribute('type', 'Number')");
         //Entering text into the password field
-        password_field.sendKeys("cosmos99");
-
+        password_field.sendKeys("cosmos");
+        Thread.sleep(3000);
         //Clicking on the login button to login to the application
         WebElement login_button=driver.findElement(By.xpath("//button[text()='Sign In']"));
-
+//        sleep(2000);
+        Thread.sleep(3000);
         //Clicking on the 'login' button
         login_button.click();
         driver.switchTo().alert().accept();
