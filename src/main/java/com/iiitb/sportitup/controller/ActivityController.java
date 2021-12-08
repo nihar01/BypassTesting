58d412d8c63f51e85ba184f1583d8b055263e68e
nihar01 <nihar_kanwar@yahoo.co.in>
2021-12-08 17:21:47 +0530

    Added activity form validation based on time field

--- src/main/java/com/iiitb/sportitup/controller/ActivityController.java
+++ src/main/java/com/iiitb/sportitup/controller/ActivityController.java
@@ -7,9 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CrossOrigin(origins= "*")
 @RestController
@@ -35,9 +40,40 @@ public class ActivityController {
         System.out.println(activity.getActivityTime());
        System.out.println( activity.getActivityDate());
         System.out.println(activity.getVenue_id());
+        String regex_time="^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+        Pattern pattern_time= Pattern.compile(regex_time);
+        Matcher matcher_time= pattern_time.matcher(activity.getActivityTime());
+        System.out.println("Activity Time check ->" +" : "+ matcher_time.matches());
+        Activity obj=null;
+        if(matcher_time.matches())
+        {
+            obj = activityService.createActivity(activity);
+            System.out.println("Activity Time check ->" +" : "+ matcher_time.matches());
+            System.out.println("Activity Created");
+//         return obj;
+        }
+        else
+        {
+            System.out.println("INVALID Time Format.Activity creation failed.");
+        }
+        return obj;
+//        String regex_date=" ^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+//        Pattern pattern_date= Pattern.compile(regex_date);
 
-        return activityService.createActivity(activity);
-    }
+//        try {
+//          Activity obj = activityService.createActivity(activity);
+//         return obj;
+//        }
+//        catch(HttpMessageNotReadableException e)
+//     {
+////            e.printStackTrace();
+//            System.out.println("Date format INVALID");
+//
+//        }
+//        return null;
+        }
+
+
 
     @GetMapping("/activity/{id}")
     public ResponseEntity<Activity> getActivityById(@PathVariable Integer id) {
